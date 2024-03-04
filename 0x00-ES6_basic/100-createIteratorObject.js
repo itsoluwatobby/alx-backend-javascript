3b859cee57495617cbc6fab88b355360c8ab9dd4
itsoluwatobby <oluwasamuel050@gmail.com>
2024-03-04 23:02:49 +0100

    lint checks completed

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,7 @@
 export default function createIteratorObject(report) {
   const employeesInDepartment = [];
-  for (const department in report.allEmployees) {
-    for (const depart of report.allEmployees[department]) {
-      employeesInDepartment.push(depart);
-    }
+  for (const department of Object.keys(report.allEmployees)) {
+    employeesInDepartment.push(...report.allEmployees[department]);
   }
   return employeesInDepartment;
 }
