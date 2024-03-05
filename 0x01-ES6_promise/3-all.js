8424a9aadad2b9064c163ddb3b5bc45a19e9e2cc
itsoluwatobby <oluwasamuel050@gmail.com>
2024-03-05 21:08:45 +0100

    completed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,8 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
-  try {
-    const [photo, user] = [await uploadPhoto(), await createUser()];
-    console.log(photo.body, `${user.firstName} ${user.lastName}`);
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((res) => {
+    console.log(res[0].body, `${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
