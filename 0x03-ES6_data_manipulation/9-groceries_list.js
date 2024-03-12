4ba0fc26fdf6464b73c2a95496257b2ffd57ee1d
itsoluwatobby <oluwasamuel050@gmail.com>
2024-03-12 21:41:47 +0100

    completed

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,12 +1,12 @@
 export default function groceriesList() {
   const fruitMap = new Map();
-  const obj = { 
-    'Apples': 10,
-    'Tomatoes': 10,
-    'Pasta': 1,
-    'Rice': 1,
-    'Banana': 5
-  }
+  const obj = {
+    Apples: 10,
+    Tomatoes: 10,
+    Pasta: 1,
+    Rice: 1,
+    Banana: 5,
+  };
   const list = Array.from(Object.keys(obj));
   list.map((item) => fruitMap.set(item, obj[item]));
   return fruitMap;
