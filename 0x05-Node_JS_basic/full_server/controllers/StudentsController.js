ff72bb98fba39941c6b91a17511ae0a323a44691
itsoluwatobby <oluwasamuel050@gmail.com>
2024-04-30 23:52:24 +0100

    refactored

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,21 +5,25 @@ class StudentsController {
     const filePath = process.argv.at(-1).split('/')[1];
     readDatabase(filePath)
       .then((data) => {
-        const result = `Number of students in CS: ${data.CS.length}. List: ${data.CS.join(', ')}\nNumber of students in SWE: ${data.SWE.length}. List: ${data.SWE.join(', ')}`;
-        response.send(`This is the list of our students\n${result}`);
+	const { CS, SWE } = data;
+        let result = `This is the list of our students\n`;
+	result += `Number of students in CS: ${CS.length}. List: ${CS.join(', ')}\n`;
+	result += `Number of students in SWE: ${SWE.length}. List: ${SWE.join(', ')}`;
+        response.status(200).send(result);
       })
       .catch((error) => response.status(500).send(error.message));
   }
 
   static getAllStudentsByMajor(request, response) {
-    if (!['CS', 'SWE'].includes(request.params.major)) {
+    const major = request.params.major.toUpperCase();
+    if (!['CS', 'SWE'].includes(major)) {
       return response.status(500).send('Major parameter must be CS or SWE');
     }
     const filePath = process.argv.at(-1).split('/')[1];
     return readDatabase(filePath)
       .then((data) => {
-        const result = `List: ${data[request.params.major].join(', ')}`;
-        return response.send(result);
+        const result = `List: ${data[major].join(', ')}`;
+        return response.status(200).send(result);
       })
       .catch((error) => response.status(500).send(error.message));
   }
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import router from './routes/index';
+import appRouter from './routes/index';
 
 const app = express();
+const hostname = '127.0.0.1';
 const PORT = 1245;
 
-app.use('/', router);
+app.use('/', appRouter);
 
-app.listen(PORT);
+app.listen(PORT, hostname);
 
 export default app;
